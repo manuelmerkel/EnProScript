a85139f65992c49982570c314a42f6550bfb7e02
manuelmerkel <manuel.merkel98@gmail.com>
2022-11-14 17:21:17 +0100

    feat: add script to update packages for python

---
+++ update_utility.sh
@@ -0,0 +1,2 @@
+sudo apt-get update -y
+sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
